97e8aeb2b4813f32c84672bedb92c4edc6e77c54
anandg112 <anandg112@gmail.com>
2016-10-28 03:02:37 +0800

    Ruby app skeleton

--- app/controllers/greeter_controller.rb
+++ app/controllers/greeter_controller.rb
@@ -1,4 +1,11 @@
 class GreeterController < ApplicationController
   def hello
+    random_names = ["Anand", "Joe", "Rick"]
+    @name = random_names.sample
+    @time = Time.now
+    @times_displayed ||= 0
+    @times_displayed += 1
   end
+  #def goodbye
+  #end
 end
